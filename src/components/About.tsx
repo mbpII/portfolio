1e75a0f0a2499d370657547cac29e8ee03fc9b29
Brianmulinge <brianjaden45@gmail.com>
2023-04-09 13:54:34 -0500

    starting from scratch again

--- src/components/About.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <section className="h-screen w-full">
-      <h1>About</h1>
-    </section>
-  );
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import Head from "next/head";
-import About from "../components/About";
-import Project from "../components/Project";
-import Contact from "../components/Contact";
-import Landing from "../components/Landing";
 
 export default function Home() {
   return (
-    <div className="bg-black text-white">
+    <div className="">
       <Head>
         <title>Brianmulinge</title>
         <meta name="description" content="My Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Landing />
-      <About />
-      <Project />
-      <Contact />
+  <div>
+    <h1 className="text-4xl font-bold">Brian Mulinge</h1>
+  </div>
     </div>
   );
 }
