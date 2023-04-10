5ce6fa7445196fcffb1f709036e1ed7e64671f79
Brianmulinge <brianjaden45@gmail.com>
2023-04-10 12:33:02 -0500

    removed all files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import { Heebo } from '@next/font/google';
+
+const heebo = Heebo({
+  subsets: ['latin'],
+  style: ['normal'],
+  weight: ['200', '300','400', '500','600','700']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <div className={heebo.className}>
+       <Component {...pageProps} />
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-  <div>
-    <h1 className="text-4xl font-bold">Brian Mulinge</h1>
-  </div>
+  <main>
+    <h1>home</h1>
+  </main>
     </div>
   );
 }
